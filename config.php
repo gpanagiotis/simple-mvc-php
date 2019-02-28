b0139cceb356b9ebc6df822ba35ccc69e2493534
Panagiotis Giannitsaros <46202117+gpanagiotis@users.noreply.github.com>
2019-02-28 16:47:47 +0200

    Add files via upload
    fix example of congiguration file

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'mvc');
+define('FILE_PATH', 'C:/wamp64/www/mvc');
+define('URL_PATH', 'http://localhost/mvc');
+define('PAGING', '5');
+define('PAGING_ITEMS', '15');
